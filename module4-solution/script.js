3e25a8f90c734169235930e2d8000e599d39461c
Gayani Kulasekara <67510691+Kulasekara@users.noreply.github.com>
2024-01-15 16:54:44 +1000

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,7 +3,7 @@ var names=["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", 
 
 for (var i =0; i < names.length; i++) {
 	if(names[i].charAt(0)=='J'|| names[i].charAt(0)=='j'){
-	    console.log("Goodbye "+ names[i])
+	    console.log("Good Bye "+ names[i])
 	}
    else{
    	   console.log("Hello "+ names[i])
