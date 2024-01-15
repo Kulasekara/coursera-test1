20d5eb1140f8b39374e6ef27ab1c6907b0c53a81
Gayani Kulasekara <67510691+Kulasekara@users.noreply.github.com>
2024-01-15 16:45:44 +1000

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,4 +1,4 @@
-var names=["jayuni","Netha","gayani","Jackson","jojo","Harini","methsith","Jeram","Lara","jimi"];
+var names=["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 
 for (var i =0; i < names.length; i++) {
